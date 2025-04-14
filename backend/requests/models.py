8747f103bc3c29b5d916408aa3c62b1e7f90e1ce
afarovruslan <afarovruslan@gmail.com>
2025-04-14 18:35:50 +0300

    Change models with files

--- backend/requests/models.py
+++ backend/requests/models.py
@@ -1,24 +1,51 @@
 from django.db import models
+import uuid
+from requests.storage_backends import UploadedStorage, EditedStorage
 
-from requests.storage_backends import PublicMediaStorage
+
+class RequestStatus(models.TextChoices):
+    WAITING = 'waiting', 'Waiting'
+    PROCESSING = 'processing', 'Processing'
+    DONE = 'done', 'Done'
 
 
 class Request(models.Model):
-    test_field = models.IntegerField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(max_length=10, choices=RequestStatus.choices,
+                              default=RequestStatus.WAITING)
+    time_end = models.DateTimeField(null=True, blank=True)
+
+    @classmethod
+    def create_request(cls):
+        return cls.objects.create()
 
     def __str__(self):
-        return str(self.test_field)
+        return str(self.id) + " — " + str(self.status)
 
 
-class Upload(models.Model):
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    file = models.FileField(storage=PublicMediaStorage())
+class UploadedFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(max_length=10, choices=RequestStatus.choices,
+                              default=RequestStatus.WAITING)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default=uuid.uuid4)
 
-class UploadedImage(models.Model):
-    image = models.ImageField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    data = models.FileField(storage=UploadedStorage())
+
+    @classmethod
+    def create_file(cls, request: Request, name: str, data):
+        id = uuid.uuid4()
+        name = str(id) + "." + name.split('.')[-1]
+        return cls.objects.create(id=id, request=request, name=name, data=data)
 
 
+class EditedFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default=uuid.uuid4)
 
+    data = models.FileField(storage=EditedStorage())
 
-
+    @classmethod
+    def create_file(cls, request: Request, name: str, data):
+        return cls.objects.create(request=request, name=name, data=data)
